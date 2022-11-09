f38af042ea9175824eb09ad850b05062dd9e0cb2
alexfilion <alexfilion@live.ca>
2022-11-09 09:54:17 -0800

    Changement fichiers service elevation ajout 2 srvs

--- paul/paul_manipulation/src/paul_gen3_manip.py
+++ paul/paul_manipulation/src/paul_gen3_manip.py
@@ -46,11 +46,27 @@ from kortex_driver.srv import *
 from kortex_driver.msg import *
 import moveit_msgs.msg
 import geometry_msgs.msg
+
 from shape_msgs.msg import MeshTriangle, Mesh, SolidPrimitive, Plane
 from math import pi, radians
 from std_srvs.srv import Empty
 from tf.transformations import quaternion_from_euler
 from copy import deepcopy
+
+from std_msgs.msg import Float64
+from sensor_msgs.msg import JointState
+from paul_manipulation.srv import ArmPosition, ArmPositionResponse
+from paul_manipulation.srv import ElevationPosition, ElevationPositionResponse
+from paul_manipulation.msg import Elevation
+
+
+
+SCAN_POSE_RIGHT = (0, 0, pi/2, pi/4, 0, pi/2)
+SCAN_POSE_LEFT  = (0, 0, pi/2, pi/4, 0, pi/2)
+BACK_POSE       = (0, 0, pi/2, pi/4, 0, pi/2)
+DROP_POSE       = (0, 0, pi/2, pi/4, 0, pi/2)
+
+
 
 class PAUL_manipulator(object):
   """PAUL_manipulator"""
@@ -60,7 +76,14 @@ class PAUL_manipulator(object):
     super(PAUL_manipulator, self).__init__()
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('PAUL_manipulator')
-    rospy.Subscriber('/arm_position_request', geometry_msgs.msg.Pose, self.request_pose_callback)
+    rospy.Subscriber('/arm_position_request', geometry_msgs.msg.Pose, self.request_pose_callback) 
+
+    self.elevationService = rospy.Service('elevation_controller_vision', ElevationPosition, self.elevationPositionCallback)
+    self.armService = rospy.Service('arm_position', ArmPosition, self.armPositionCallback)
+
+    self.height = 0
+    self.subHeight = rospy.Subscriber("/joint_states", JointState, self.GetHeightElevation)
+
 
     try:
       self.is_gripper_present = rospy.get_param(rospy.get_namespace() + "is_gripper_present", False)
@@ -125,7 +148,7 @@ class PAUL_manipulator(object):
     # Execute the trajectory and block while it's not finished
     return arm_group.execute(planned_path1, wait=True)
 
-  def reach_joint_angles(self, tolerance):
+  def reach_joint_angles(self, J1, J2, J3, J4, J5, J6, tolerance):
     arm_group = self.arm_group
     success = True
 
@@ -138,21 +161,12 @@ class PAUL_manipulator(object):
     self.arm_group.set_goal_joint_tolerance(tolerance)
 
     # Set the joint target configuration
-    if self.degrees_of_freedom == 7:
-      joint_positions[0] = pi/2
-      joint_positions[1] = 0
-      joint_positions[2] = pi/4
-      joint_positions[3] = -pi/4
-      joint_positions[4] = 0
-      joint_positions[5] = pi/2
-      joint_positions[6] = 0.2
-    elif self.degrees_of_freedom == 6:
-      joint_positions[0] = 0
-      joint_positions[1] = 0
-      joint_positions[2] = pi/2
-      joint_positions[3] = pi/4
-      joint_positions[4] = 0
-      joint_positions[5] = pi/2
+    joint_positions[0] = J1
+    joint_positions[1] = J2
+    joint_positions[2] = J3
+    joint_positions[3] = J4
+    joint_positions[4] = J5
+    joint_positions[5] = J6
     arm_group.set_joint_value_target(joint_positions)
     
     # Plan and execute in one command
@@ -261,10 +275,7 @@ class PAUL_manipulator(object):
 
 
     rospy.loginfo("Reaching requested Pose..." + str(pose_msg))
-    
-    actual_pose = self.get_cartesian_pose()
     actual_pose.position.x += pose_msg.position.x
-    # actual_pose = GeneratePose(pose_msg.position.x, pose_msg.position.y, pose_msg.position.z, 90.0, 0.0, 80.0)
     
     success = self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
     
@@ -279,188 +290,188 @@ class PAUL_manipulator(object):
 
     rospy.loginfo("Request is a " + str(success))
 
-def example_send_gripper_command(value):
-  # Initialize the request
-  # Close the gripper
-  req = SendGripperCommandRequest()
-  finger = Finger()
-  finger.finger_identifier = 0
-  finger.value = value
-  req.input.gripper.finger.append(finger)
-  req.input.mode = GripperMode.GRIPPER_FORCE
+  def example_send_gripper_command(value):
+    # Initialize the request
+    # Close the gripper
+    req = SendGripperCommandRequest()
+    finger = Finger()
+    finger.finger_identifier = 0
+    finger.value = value
+    req.input.gripper.finger.append(finger)
+    req.input.mode = GripperMode.GRIPPER_FORCE
 
-  robot_name = rospy.get_param('~robot_name', "my_gen3_lite")
-  send_gripper_command_full_name = '/' + robot_name + '/base/send_gripper_command'
-  send_gripper_commandCallback = rospy.ServiceProxy(send_gripper_command_full_name, SendGripperCommand)
+    robot_name = rospy.get_param('~robot_name', "my_gen3_lite")
+    send_gripper_command_full_name = '/' + robot_name + '/base/send_gripper_command'
+    send_gripper_commandCallback = rospy.ServiceProxy(send_gripper_command_full_name, SendGripperCommand)
 
-  rospy.loginfo("Sending the gripper command...")
+    rospy.loginfo("Sending the gripper command...")
 
-  # Call the service 
-  try:
-    send_gripper_commandCallback(req)
-  except rospy.ServiceException:
-    rospy.logerr("Failed to call SendGripperCommand")
-    return False
-  else:
-    time.sleep(0.5)
-    return True
+    # Call the service 
+    try:
+      send_gripper_commandCallback(req)
+    except rospy.ServiceException:
+      rospy.logerr("Failed to call SendGripperCommand")
+      return False
+    else:
+      time.sleep(0.5)
+      return True
 
-def GeneratePose(x, y, z, theta, phi, psi):
-  actual_pose = geometry_msgs.msg.Pose()
-  actual_pose.position.x = x
-  actual_pose.position.y = y
-  actual_pose.position.z = z
+  def GeneratePose(x, y, z, theta, phi, psi):
+    actual_pose = geometry_msgs.msg.Pose()
+    actual_pose.position.x = x
+    actual_pose.position.y = y
+    actual_pose.position.z = z
 
-  theta_x = radians(theta)
-  theta_y = radians(phi)
-  theta_z = radians(psi)
+    theta_x = radians(theta)
+    theta_y = radians(phi)
+    theta_z = radians(psi)
 
-  q = quaternion_from_euler(theta_x, theta_y, theta_z)
-  
-  actual_pose.orientation.x = q[0]
-  actual_pose.orientation.y = q[1]
-  actual_pose.orientation.z = q[2]
-  actual_pose.orientation.w = q[3]
-  
-  return actual_pose
+    q = quaternion_from_euler(theta_x, theta_y, theta_z)
+    
+    actual_pose.orientation.x = q[0]
+    actual_pose.orientation.y = q[1]
+    actual_pose.orientation.z = q[2]
+    actual_pose.orientation.w = q[3]
+    
+    return actual_pose
+
+
+
+
+  # For moving the elevation system with the arm in the scan position (safe position)
+  def elevationPositionCallback(self, msg): 
+    #if msg.level.data < 0 or msg.level.data > 2:
+    if msg.level < 0 or msg.level > 2:
+      return ElevationPositionResponse(False)
+
+    robot = PAUL_manipulator()
+    success = robot.is_init_success
+    try:
+      rospy.delete_param("/kortex_examples_test_results/moveit_general_python")
+    except:
+      return ElevationPositionResponse(False)
+
+    # Create the curtain for the whole cart
+    robot.add_box(0.0,-0.25,0.0, 1.0, (0.7, 0.2, 1.5), box_name='cartCurtain')
+
+    if success:
+      rospy.loginfo("Reaching Scan Pose...")
+      success &= self.reach_joint_angles(SCAN_POSE_RIGHT, tolerance=0.01)
+      print(success)
+
+    # Remove the curtain
+    robot.remove_box('cartCurtain')
+
+    # Call service elevation_controller_vision
+    rospy.wait_for_service('elevation_controller_vision')
+    try:
+      elevationServiceResponse = rospy.ServiceProxy('/elevation_controller_vision', ElevationPosition)
+      elevation = Elevation()
+      elevation.data = msg
+      resp1 = elevationServiceResponse(elevation)
+      print("Service answer: " )
+      print(resp1)
+    except rospy.ServiceException as e:
+      print("Service call failed: %s"%e)
+      return ElevationPositionResponse(False)
+
+    return ElevationPositionResponse(True)
+
+
+
+  def GetHeightElevation(self, msg):
+    self.height = msg.position[0]
+
+  # For moving the arm to the requested position
+  def armPositionCallback(self, pose_msg):
+    # Height of elevation system
+
+    if self.height > 700:
+      self.add_box()
+    elif self.height > 300:
+      self.add_box()
+    else:
+      self.add_box()
+
+    rospy.loginfo("Reaching requested Z-Pose..." + str(pose_msg))
+    actual_pose = self.get_cartesian_pose()
+    actual_pose.position.y += pose_msg.position.y
+    actual_pose.position.z += pose_msg.position.z
+    
+    success = self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+
+    rospy.loginfo("Reaching requested Pose..." + str(pose_msg))
+    actual_pose.position.x += pose_msg.position.x
+    
+    success = self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+
+    if success:
+      rospy.loginfo("Closing the gripper...")
+      success &= self.reach_gripper_position(0.65)
+
+    if success:
+      rospy.loginfo("taking article...")
+      actual_pose.position.z += 0.025
+      success = self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+
+    rospy.loginfo("Request is a " + str(success))
+
+    return ArmPositionResponse(True)
+
+
+
 
 def main():
-  robot = PAUL_manipulator()
-  # robot.add_box(0.0,0.0,0.0, 1.0, (0.2, 0.2, 0.0), box_name='table')
-  robot.remove_box('table')
-  robot.remove_box()
-  # robot.add_box(0.0, -0.050, 0.10, 1.0, (0.4, 0.01, 0.2))
-  # try:
-  #   robot.remove_box()
-  # except:
-  #   pass
-  # rospy.loginfo("Reaching Named Target Home...")
-  # success = robot.reach_named_position("home")
-  # print(success)
+    robot = PAUL_manipulator()
+    # robot.add_box(0.0,0.0,0.0, 1.0, (0.2, 0.2, 0.0), box_name='table')
+    robot.remove_box('table')
+    robot.remove_box()
+    # robot.add_box(0.0, -0.050, 0.10, 1.0, (0.4, 0.01, 0.2))
+    # try:
+    #   robot.remove_box()
+    # except:
+    #   pass
+    # rospy.loginfo("Reaching Named Target Home...")
+    # success = robot.reach_named_position("home")
+    # print(success)
 
-  print("Awaiting requests...")
-  rospy.spin()
+    print("Awaiting requests...")
+    rospy.spin()
 
 
 def test():
-  example = PAUL_manipulator()
+    example = PAUL_manipulator()
 
-  # For testing purposes
-  success = example.is_init_success
-  try:
-      rospy.delete_param("/kortex_examples_test_results/moveit_general_python")
-  except:
-      pass
+    # For testing purposes
+    success = example.is_init_success
+    try:
+        rospy.delete_param("/kortex_examples_test_results/moveit_general_python")
+    except:
+        pass
 
-#  if success:
-#    rospy.loginfo("Reaching Named Target Vertical...")
-#    success &= example.reach_named_position("vertical")
-#    print (success)
-  
-#  if success:
-#    rospy.loginfo("Reaching Joint Angles...")  
-#    success &= example.reach_joint_angles(tolerance=0.01) #rad
-#    print (success)
-  
-#  if success:
-#    rospy.loginfo("Reaching Named Target Home...")
-#    success &= example.reach_named_position("home")
-#    print (success)
+    example.add_box(0.0, 0.0, 0.0, 1.0, (0.2, 0.2, 0.2))
 
-#  if success:
-#    rospy.loginfo("Reaching Cartesian Pose...")
-    
-#    actual_pose = example.get_cartesian_pose()
-#    actual_pose.position.z -= 0.2
-#    success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
-#    print (success)
-    
-#  if example.degrees_of_freedom == 7 and success:
-#    rospy.loginfo("Reach Cartesian Pose with constraints...")
-#    # Get actual pose
-#    actual_pose = example.get_cartesian_pose()
-#    actual_pose.position.y -= 0.3
-    
-#    # Orientation constraint (we want the end effector to stay the same orientation)
-#    constraints = moveit_msgs.msg.Constraints()
-#    orientation_constraint = moveit_msgs.msg.OrientationConstraint()
-#    orientation_constraint.orientation = actual_pose.orientation
-#    constraints.orientation_constraints.append(orientation_constraint)
+    # if success:
+    #   rospy.loginfo("Reaching 1st Pose...")
+    #   actual_pose = GeneratePose(0.5, 0.2, 0.1, 90, 0, 90)
+    #   success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+    #   print(success)
 
-#    # Send the goal
-#    success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=constraints)
-  
-  if not example.box_is_in_scene('box'):
-    box_added = example.add_box(0.467, 0.125, 0.133, 1.0, (0.4, 0.01, 0.2))
-  # if not example.box_is_in_scene('table'):
-  example.add_box(0.0,0.0,0.0, 1.0, (2.0, 2.0, 0.01), box_name='table')
+    # if success:
+    #   rospy.loginfo("Reaching over box...")
+    #   actual_pose = GeneratePose(0.5, 0.5, 0.1, 90, 0, 90)
+    #   success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+    #   print(success)
 
-  if success:
-    rospy.loginfo("Reaching 1st Pose...")
-    actual_pose = GeneratePose(0.546, 0.236, 0.132, 91.7, -1.8, 86.1)
-    success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
-    print (success)
 
-  if success:
-    rospy.loginfo("Reaching over box...")
-    actual_pose = GeneratePose(0.498, -0.059, 0.132, 91.7, -2.0, 86.1)
-    success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
-    print (success)
 
-  if example.is_gripper_present and success:
 
-    rospy.loginfo("Opening the gripper...")
-    success &= example.reach_gripper_position(0.99)
-    rospy.loginfo("Closing the gripper...")
-    success &= example.reach_gripper_position(0.35)
-    #success &= example_send_gripper_command(1)
 
-    #rospy.loginfo("Opening the gripper...")
-    #success &= example.reach_gripper_position(0.99)
+    # For testing purposes
+    rospy.set_param("/kortex_examples_test_results/moveit_general_python", success)
 
-  # if success:
-  #   rospy.loginfo("Reaching 2st Pose...")
-  #   poses = []
-  #   poses.append(GeneratePose(0.476, 0.122, 0.017, 88.8, -1.5, 50.5))
-  #   poses.append(GeneratePose(0.476, 0.122, 0.196, 88.8, -1.5, 50.5))
-  #   poses.append(GeneratePose(0.320, 0.108, 0.148, 88.8, -1.5, 50.5))
-  #   poses.append(GeneratePose(0.320, 0.108, 0.117, 88.8, -1.5, 50.5))
-
-  #   success = True
-  #   for i in range(len(poses)):
-  #     success = example.reach_cartesian_pose(pose=poses[i], tolerance=0.01, constraints=None)
-  #     rospy.loginfo("pose " + str(i))
-  #     if not success:
-  #       break
-
-  #   rospy.loginfo("Opening the gripper...")
-  #   success &= example.reach_gripper_position(0.99)
-  #   pose = GeneratePose(0.320, 0.108, 0.127, 88.8, -1.5, 50.5)
-    
-  #   success &= example.reach_cartesian_pose(pose=pose, tolerance=0.01, constraints=None)
-  #   print("After planning group function")
-  #   print (success)
-
-  if success:
-    rospy.loginfo("Removing table box. Reaching last Poses...")
-    example.remove_box('table')
-    example.add_box(0.0,0.0,0.0, 1.0, (2.0, 0.5, 0.01), box_name='table')
-    actual_pose = GeneratePose(0.025, 0.384, -0.163, 177.7, 0.9, 83.2)
-    success &= example.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
-    print (success)
-
-  if example.is_gripper_present and success:
-
-    rospy.loginfo("Opening the gripper...")
-    success &= example.reach_gripper_position(0.99)
-    rospy.loginfo("Closing the gripper...")
-    success &= example.reach_gripper_position(0.35)
-
-  # For testing purposes
-  rospy.set_param("/kortex_examples_test_results/moveit_general_python", success)
-
-  if not success:
-      rospy.logerr("The example encountered an error.")
+    if not success:
+        rospy.logerr("The example encountered an error.")
 
 if __name__ == '__main__':
   main()
