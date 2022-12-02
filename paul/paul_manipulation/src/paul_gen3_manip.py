5e3698590e21c5eb6622d0c57aaa75d12296fdee
Victoria <victoria.pitz.clairoux@usherbrooke.ca>
2022-12-01 19:09:34 -0500

    megageniale

--- paul/paul_manipulation/src/paul_gen3_manip.py
+++ paul/paul_manipulation/src/paul_gen3_manip.py
@@ -92,8 +92,8 @@ SCAN_POSE_5_LEFT  = (0, 0, pi/2, pi/4, 0, pi/2)
 BACK_POSE         = (0, 0, pi/2, pi/4, 0, pi/2)
 DROP_POSE         = (0, 0, pi/2, pi/4, 0, pi/2)
 
-DISTANCE_SHELF_X = 0.53 # barre d'acier a l'etagere (m)
-DISTANCE_SHELF_Y =  0.095 # milieu de l'etagere vs milieu bras (m)
+DISTANCE_SHELF_X = 0.40 # barre d'acier a l'etagere (m)
+DISTANCE_SHELF_Y =  0.06 # milieu de l'etagere vs milieu bras (m)
 
 
 class PAUL_manipulator(object):
@@ -469,7 +469,8 @@ class PAUL_manipulator(object):
   def armPositionGrabCallback(self, pose_msg):
 
     rospy.loginfo("Opening the gripper...")
-    if pose_msg.grip > 0.5:
+    if pose_msg.grip > 0.7:
+      print(pose_msg.grip)
       self.reach_gripper_position(0.2)
     else:
       self.reach_gripper_position(0)
@@ -483,34 +484,36 @@ class PAUL_manipulator(object):
     actual_pose.position.z += pose_msg.pose.position.z
     actual_pose.position.x += pose_msg.pose.position.x/2
     
-    success = self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None) # upped tolerance, will be more precise in next step
-
-    # remove full shelf
-    self.remove_box('Shelf')
-
-    rospy.loginfo("Reaching Article..." + str(pose_msg.pose))
-    actual_pose.position.x += pose_msg.pose.position.x/2
-    
-    success &= self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+    success = self.reach_cartesian_pose(pose=actual_pose, tolerance=0.02, constraints=None) # upped tolerance, will be more precise in next step
 
     if success:
+
+      # remove full shelf
+      self.remove_box('Shelf')
+      rospy.loginfo("Reaching Article..." + str(pose_msg.pose))
+      actual_pose.position.x += pose_msg.pose.position.x/2
+      
+      success &= self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+
       rospy.loginfo("Closing the gripper...")
       success_grip = self.reach_gripper_position(pose_msg.grip)
 
-    # if success:
-    rospy.loginfo("taking article...")
-    actual_pose.position.z += 0.02
-    actual_pose.position.x -= pose_msg.pose.position.x/2
-    success &= self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+      # if success:
+      rospy.loginfo("taking article...")
+      actual_pose.position.z += 0.02
+      actual_pose.position.x -= pose_msg.pose.position.x/2
+      success &= self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
 
-    actual_pose.position.x -= pose_msg.pose.position.x/2
-    success &= self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
+      actual_pose.position.z += 0.01
+      actual_pose.position.x -= pose_msg.pose.position.x/2
+      success &= self.reach_cartesian_pose(pose=actual_pose, tolerance=0.01, constraints=None)
 
-    # re-add shelf but thicker to compensate for article in hand
-    self.add_box(-0.460 - DISTANCE_SHELF_X, DISTANCE_SHELF_Y, -0.185, 0.0, (0.460, 0.910, 1.830), box_name='Shelf')
-    self.zones_list.append("Shelf")
+      # re-add shelf but thicker to compensate for article in hand
+      self.add_box(-0.40 - DISTANCE_SHELF_X, DISTANCE_SHELF_Y, -0.185, 0.0, (0.54, 3, 3), box_name='Shelf_items')
+      self.zones_list.append("Shelf_items")
 
-    success &= self.drop_sequence()
+      if success:
+        success &= self.drop_sequence()
 
     rospy.loginfo("Request is a " + str(success))
 
@@ -520,7 +523,7 @@ class PAUL_manipulator(object):
   # service
   def armPositionJointCallback(self, joints):
 
-      self.add_all_security_zones()
+      # self.add_all_security_zones()
       
       success = self.reach_joint_angles(joints.joints[0], joints.joints[1], joints.joints[2], joints.joints[3], joints.joints[4], joints.joints[5], tolerance=0.01)
 
@@ -530,21 +533,83 @@ class PAUL_manipulator(object):
 
 
   # For moving the arm to the requested position in cartesian
-  def armPositionCartesianCallback(self, pose_msg):
+  def armPositionCartesianCallback(self, msg):
       success = True
 
       self.add_all_security_zones()
+      pose_msg = geometry_msgs.msg.Pose()
+
+      if msg.level == 1:
+          if msg.searchPos == 0:
+              pose_msg.position.x = -0.13 - DISTANCE_SHELF_X
+              pose_msg.position.y = -0.06 + DISTANCE_SHELF_Y
+              pose_msg.position.z = -0.203
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+          if msg.searchPos == 1:
+              pose_msg.position.x = -0.12 -DISTANCE_SHELF_X
+              pose_msg.position.y = 0.22 + DISTANCE_SHELF_Y
+              pose_msg.position.z = -0.216
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+      if msg.level == 2:
+          if msg.searchPos == 0:
+              pose_msg.position.x = -0.14 - DISTANCE_SHELF_X
+              pose_msg.position.y = -0.01 + DISTANCE_SHELF_Y
+              pose_msg.position.z = 0.085
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+          elif msg.searchPos == 1:
+              pose_msg.position.x = -0.12 - DISTANCE_SHELF_X
+              pose_msg.position.y = 0.25 + DISTANCE_SHELF_Y
+              pose_msg.position.z = 0.08
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+      elif msg.level == 3:
+          if msg.searchPos == 0:
+              pose_msg.position.x = -0.05 - DISTANCE_SHELF_X
+              pose_msg.position.y = -0.20 + DISTANCE_SHELF_Y 
+              pose_msg.position.z = 0.344
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+          elif msg.searchPos == 1:
+              pose_msg.position.x = -0.08 - DISTANCE_SHELF_X
+              pose_msg.position.y = 0.17 + DISTANCE_SHELF_Y
+              pose_msg.position.z = 0.34
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+      elif msg.level == 4:
+          if msg.searchPos == 0:
+              pose_msg.position.x = -0.10 - DISTANCE_SHELF_X
+              pose_msg.position.y = -0.19 + DISTANCE_SHELF_Y
+              pose_msg.position.z = 0.60
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
+          elif msg.searchPos == 1:
+              pose_msg.position.x = -0.10 - DISTANCE_SHELF_X
+              pose_msg.position.y = 0.25 + DISTANCE_SHELF_Y
+              pose_msg.position.z = 0.60
+              pose_msg.orientation.x = -90
+              pose_msg.orientation.y = -179
+              pose_msg.orientation.z = 90
 
       actual_pose = self.get_cartesian_pose()
       goal_pose = geometry_msgs.msg.Pose()
 
-      goal_pose.position.y = pose_msg.pose.position.y
-      goal_pose.position.z = pose_msg.pose.position.z
-      goal_pose.position.x = pose_msg.pose.position.x
+      goal_pose.position.y = pose_msg.position.y
+      goal_pose.position.z = pose_msg.position.z
+      goal_pose.position.x = pose_msg.position.x
  
-      roll = radians(pose_msg.pose.orientation.x)
-      pitch = radians(pose_msg.pose.orientation.y)
-      yaw = radians(pose_msg.pose.orientation.z)
+      roll = radians(pose_msg.orientation.x)
+      pitch = radians(pose_msg.orientation.y)
+      yaw = radians(pose_msg.orientation.z)
 
       goal_pose.orientation.y = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
       goal_pose.orientation.z = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
@@ -563,13 +628,11 @@ class PAUL_manipulator(object):
 
       self.add_all_security_zones()
 
-      # success = self.reach_joint_angles(radians(-(360-329.84)),radians(-(360-354.67)), radians(134.11), radians(1.55), radians(-(360-274.08)), radians(-(360-298.95)), tolerance=0.01)
-
       success = self.reach_joint_angles(radians(-(360-272.01)),radians(-(360-285.38)), radians(100.14), radians(21.11), radians(-(360-340.58)), radians(-(360-303.13)), tolerance=0.01)
       
       self.reach_gripper_position(0)
       
-      success = self.reach_joint_angles(radians(-(360-329.84)),radians(-(360-354.67)), radians(134.11), radians(1.55), radians(-(360-274.08)), radians(-(360-298.95)), tolerance=0.01)
+      success = self.reach_joint_angles(radians(-(360-329)),radians(-(360-341.93)), radians(132.88), radians(-(360-326.03)), radians(-(360-296.1)), radians(-(360-282.45)), tolerance=0.01)
 
       return success
 
@@ -580,25 +643,41 @@ class PAUL_manipulator(object):
       del self.zones_list[:]
 
       # Adding the fixed box which protect the arm from the elevation system in steel
-      self.add_box(0.0, 0.0, -0.1075, 1.0, (0.35, 0.1, 0.18), box_name="Elevation_system_steel")
+      self.add_box(0.0, 0.0, -0.1075, 1.0, (0.31, 0.1, 0.18), box_name="Elevation_system_steel")
       self.zones_list.append("Elevation_system_steel")
+      # vis et shaft
+      self.add_box(0.0, -0.08, -0.5, 1.0, (0.31, 0.10, 1.2), box_name="Elevation_vis")
+      self.zones_list.append("Elevation_vis")
       # shelf
-      self.add_box(-0.460 - DISTANCE_SHELF_X, DISTANCE_SHELF_Y, -0.185, 0.0, (0.360, 0.910, 1.830), box_name='Shelf')
+      self.add_box(-0.44 - DISTANCE_SHELF_X, DISTANCE_SHELF_Y, -0.185, 0.0, (0.36, 3, 2.5), box_name='Shelf')
       self.zones_list.append("Shelf")
 
       # Create the curtain for the whole cart
       self.add_box(0.0, -0.18, - 0.42, 0.0, (0.63, 0.17, 1.31), box_name='Cart_curtain')
       self.zones_list.append("Cart_curtain")
+
+      # mur lol
+      self.add_box(0.0, 0.60, 0.0, 0.0, (3, 0.02, 3), box_name='table')
+      self.zones_list.append("table")
+
+      # faux toit
+      self.add_box(0.0, 0.0, 1.0, 0.0, (3, 3, 0.01), box_name='roff')
+      self.zones_list.append("roof")
+
+
       # drop zone #34x34x34 ish
       # bout a 90 cm du robot
       # cart left side
-      self.add_box(0.17, -0.45, -0.57, 0.0, (0.03, 0.90, 1.05), box_name='cart_left')
+      self.add_box(0.17, -0.45, -0.57, 0.0, (0.03, 1.5, 1.05), box_name='cart_left')
       self.zones_list.append("cart_left")
+      # cart left side - wider
+      self.add_box(0.27, -0.45, -0.57, 0.0, (0.20, 2.5, 1.0), box_name='cart_left_extra')
+      self.zones_list.append("cart_left_extra")
       # cart right side
       self.add_box(-0.17, -0.45, -0.57, 0.0, (0.03, 0.90, 1.05), box_name='cart_right')
       self.zones_list.append("cart_right")
       # cart right side - wider
-      self.add_box(-0.27, -0.45, -0.57, 0.0, (0.20, 0.90, 1.05), box_name='cart_right_extra')
+      self.add_box(-0.27, -0.45, -0.57, 0.0, (0.20, 0.75, 1.05), box_name='cart_right_extra')
       self.zones_list.append("cart_right_extra")
       # cart back side
       self.add_box(0.0, -0.24, -0.57, 0.0, (0.60, 0.20, 1.05), box_name='cart_back')
@@ -609,6 +688,7 @@ class PAUL_manipulator(object):
       # cart bottom
       self.add_box(0.0, -0.45, -0.410, 0.0, (0.50, 0.90, 0.05), box_name='cart_bottom')
       self.zones_list.append("cart_bottom")
+      
 
 def main():
     robot = PAUL_manipulator()
